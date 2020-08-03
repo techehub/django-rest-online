c20753434815ebe6f1abcd4bb9a1dbf59bd33933
vijeesh <vijeesh.tp@expertzlab.com>
2020-08-03 17:00:31 +0530

    august 3

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, ProfileDetailView
 
 urlpatterns = [
-    path ('profile', ProfileView.as_view() )
+    path ('profile', ProfileView.as_view() ),
+    path('profile/<str:id>', ProfileDetailView.as_view())
 ]
 
 
--- profileapp/views.py
+++ profileapp/views.py
@@ -3,7 +3,49 @@ from rest_framework.decorators import  APIView
 # Create your views here.
 from .models import Profile
 from .serializers import ProfileSerializer
-from django.http  import HttpResponse
+from  rest_framework.response  import Response
+
+class InvalidProfileErr(Exception):
+    pass
+
+class ProfileDetailView (APIView):
+
+    def getdata (self, id):
+        try:
+            profile = Profile.objects.get (id=  id)
+        except:
+            raise InvalidProfileErr ( )
+
+    def get (self, req, id):
+        try:
+            profile =self.getdata(id)
+            serializer = ProfileSerializer( profile)
+            return Response(serializer.data)
+        except InvalidProfileErr as e:
+            print (e)
+            return Response("Invalid profile id")
+
+    def put (self, req, id):
+
+        try :
+            profile = self.getdata(id)
+            serializer = ProfileSerializer(profile, req.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+        except InvalidProfileErr as e:
+            print(e)
+            return Response("Invalid profile id")
+
+    def delete (self, req, id):
+        try:
+            profile =self.getdata(id)
+            profile.delete()
+            return Response("deleted")
+        except InvalidProfileErr as e:
+            print(e)
+            return Response("Invalid profile id")
+
 
 class ProfileView (APIView):
 
@@ -11,11 +53,16 @@ class ProfileView (APIView):
         profiles= Profile.objects.all ()
         if profiles:
             serializer = ProfileSerializer(profiles, many=True)
-            #if (serializer.is_valid()):
-            return HttpResponse(serializer.data)
+            return Response(serializer.data)
         else :
-            return HttpResponse('none')
+            return Response('none')
 
 
-    def post (self):
-        pass
+    def post (self, request):
+        print (request.data)
+        serializer = ProfileSerializer (data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else :
+            return Response(serializer.errors)
